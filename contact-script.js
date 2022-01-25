810ecf1643acf9fb0a9f40a308e0a0f5d6830a69
Geoff-Walker <geoffwalker1980@gmail.com>
2022-01-25 15:23:20 +0000

    desktop layout complete form complete

--- contact-script.js
+++ contact-script.js
@@ -21,3 +21,84 @@ function setRotation(hand, rotationRatio) {
 }
 
 setClock();
+
+coffeeCup = document.querySelector('.coffee-cup-container');
+folderFront = document.querySelector('.folder-front');
+folder = document.querySelector('.folder');
+tab = document.querySelector('.tab');
+folderText = document.querySelector('.folder-text-hide');
+contactForm = document.querySelector('#contact-form');
+right = document.querySelector('.right');
+left = document.querySelector('.left');
+bottom = document.querySelector('.bottom');
+submit = document.querySelector('#submit');
+tabText = document.querySelector('#tab');
+
+folderFront.addEventListener('click', folderMotion);
+folderFront.addEventListener('mouseover', folderMotion);
+folderFront.addEventListener('ontouch', folderMotion);
+submit.addEventListener('click', folderMotion);
+
+function testing() {
+	folder.classList.add('hidden');
+}
+
+function folderMotion() {
+	if (!folder.classList.contains('folder-up')) {
+		folderFront.classList.add('folder-front-up');
+		folder.classList.add('folder-up');
+		tab.classList.add('tab-up');
+		folderFront.removeEventListener('mouseover', folderMotion);
+		folderFront.removeEventListener('ontouch', folderMotion);
+		folderFront.removeEventListener('click', folderMotion);
+		left.addEventListener('click', folderMotion);
+		bottom.addEventListener('click', folderMotion);
+
+		setTimeout((e) => {
+			contactForm.classList.remove('hidden');
+			tabText.innerText = 'X - Close Form';
+			tabText.addEventListener('click', folderMotion);
+			folderText.classList.add('hidden');
+			folderFront.classList.add('folder-front-rotate');
+			folderFront.classList.remove('folder-front-rotate-return');
+		}, 500);
+
+		setTimeout((e) => {
+			folderFront.classList.add('hidden');
+		}, 1500);
+	} else {
+		folderFront.classList.remove('hidden');
+		folderFront.classList.remove('folder-front-rotate');
+		folderFront.classList.add('folder-front-rotate-return');
+		tabText.innerText = 'Contact Form';
+
+		left.removeEventListener('click', folderMotion);
+		bottom.removeEventListener('click', folderMotion);
+
+		setTimeout((e) => {
+			folderText.classList.remove('hidden');
+		}, 700);
+
+		setTimeout((e) => {
+			contactForm.classList.add('hidden');
+		}, 1000);
+
+		setTimeout((e) => {
+			folderFront.classList.remove('folder-front-up');
+			folderFront.classList.remove('folder-front-rotate-return');
+			folder.classList.remove('folder-up');
+			tab.classList.remove('tab-up');
+			folderFront.addEventListener('click', folderMotion);
+			folderFront.addEventListener('mouseover', folderMotion);
+			folderFront.addEventListener('ontouch', folderMotion);
+		}, 1500);
+	}
+
+	// if (!folderFront.classList.contains('folder-front-rotate')) {
+	// 	folderFront.classList.add('folder-front-rotate');
+	// 	folderFront.classList.remove('folder-front-rotate-return');
+	// } else {
+	// 	folderFront.classList.remove('folder-front-rotate');
+	// 	folderFront.classList.add('folder-front-rotate-return');
+	// }
+}
