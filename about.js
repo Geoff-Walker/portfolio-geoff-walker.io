1a42a34b481e26311b8de2e452dc44f84dff84c7
Geoff-Walker <geoffwalker1980@gmail.com>
2022-02-01 13:13:31 +0000

    about skills section

--- about.js
+++ about.js
@@ -4,7 +4,13 @@ navContact = document.querySelector('.contact-rotate');
 navHomeImage = document.querySelector('.nav-home-image-reveal');
 navProjectImage = document.querySelector('.nav-project-image-reveal');
 navContactImage = document.querySelector('.nav-contact-image-reveal');
+navHomeClick = document.querySelector('.nav-home-click-reveal');
+navProjectClick = document.querySelector('.nav-project-click-reveal');
+navContactClick = document.querySelector('.nav-contact-click-reveal');
 navImage = document.querySelector('.nav-image');
+resumeLogo = document.querySelector('.logo-9');
+linkedInLogo = document.querySelector('.logo-10');
+gitHubLogo = document.querySelector('.logo-11');
 
 navHome.addEventListener('mouseover', navHoverHome);
 navHome.addEventListener('mouseout', navHoverHomeRem);
@@ -12,7 +18,7 @@ navProj.addEventListener('mouseover', navHoverProject);
 navProj.addEventListener('mouseout', navHoverProjectRem);
 navContact.addEventListener('mouseover', navHoverContact);
 navContact.addEventListener('mouseout', navHoverContactRem);
-navImage.addEventListener('click', goHome);
+navImage.addEventListener('click', returnHome);
 
 function navHoverHome() {
 	navHomeImage.classList.remove('nav-hidden');
@@ -50,13 +56,45 @@ function navHoverContactRem() {
 }
 
 function goHome() {
-	window.location.href = 'index.html';
+	navHomeClick.classList.remove('nav-hidden');
+	navHomeClick.classList.add('highlight-blue');
+	setTimeout(() => {
+		window.location.href = 'index.html';
+	}, 1000);
 }
 
 function goProjects() {
-	window.location.href = 'project.html';
+	navProjectClick.classList.remove('nav-hidden');
+	navProjectClick.classList.add('highlight-blue');
+	setTimeout(() => {
+		window.location.href = 'project.html';
+	}, 1000);
 }
 
 function goContact() {
-	window.location.href = 'contact.html';
+	navContactClick.classList.remove('nav-hidden');
+	navContactClick.classList.add('highlight-blue');
+	setTimeout(() => {
+		window.location.href = 'contact.html';
+	}, 1000);
+}
+
+function returnHome() {
+	window.location.href = 'index.html';
+}
+
+resumeLogo.addEventListener('click', openResume);
+linkedInLogo.addEventListener('click', openLinkdIn);
+gitHubLogo.addEventListener('click', openGithub);
+
+function openResume() {
+	window.open('https://www.geoff-walker.io/Resume.pdf');
+}
+
+function openLinkdIn() {
+	window.open('https://www.linkedin.com/in/geoff-walker-a3ab02227/');
+}
+
+function openGithub() {
+	window.open('https://github.com/Geoff-Walker/Geoff-Walker');
 }
